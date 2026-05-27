9a9a275701dae238343a2b506a3d8cb9bdbd35aa
Colin B. Chin Choy <cchinchoy@gmail.com>
2026-05-27 16:14:12 -0400

    init

---
+++ apps/api/src/server.ts
@@ -0,0 +1,19 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+const PORT = 3001;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/api/health", (req,res) => {
+    res.json({
+        status: "ok",
+        service: "youtube-downloader-api",
+    });
+});
+
+app.listen(PORT, ()=> {
+    console.log(`API running on http://localhost:${PORT}`);
+});
